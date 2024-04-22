c9f86befa5e705e3b93ad4b7cf75996f3d2f36ef
ilvoron <46526864+ilvoron@users.noreply.github.com>
2024-04-22 22:46:15 +0300

    Deleted *.bat files

--- _build_cmake_make.bat
+++
@@ -1,8 +0,0 @@
-echo off
-echo Changing directory to "build"
-cd build
-echo Running CMake
-cmake -G "MSYS Makefiles" ..
-echo Running Make
-make
-set /p DUMMY=Done. Press ENTER to exit...
--- _build_make.bat
+++
@@ -1,6 +0,0 @@
-echo off
-echo Changing directory to "build"
-cd build
-echo Running Make
-make
-set /p DUMMY=Done. Press ENTER to exit...
